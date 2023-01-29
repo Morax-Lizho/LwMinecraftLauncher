c86f8613dfe0d133ff3beb624120c28509e349f1
Morax-Lizho <259333592@qq.com>
2023-01-29 19:44:55 +0800

    完成基本启动功能

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,17 +12,40 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Panuon.UI.Silver;
+using KMCCC.Launcher;
+using KMCCC.Authentication;
 
 namespace LwMinecraftLauncher
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : WindowX
     {
+        public static LauncherCore Core = LauncherCore.Create();
         public MainWindow()
         {
             InitializeComponent();
+            var versions = Core.GetVersions().ToArray();
+            versionCombo.ItemsSource = versions;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Core.JavaPath = @"D:\Java\java8\bin\javaw.exe";
+            var ver = (KMCCC.Launcher.Version)versionCombo.SelectedItem;
+            var result = Core.Launch(new LaunchOptions
+            {
+                Version = ver, //Ver为Versions里你要启动的版本名字
+                MaxMemory = 1024, //最大内存，int类型
+                Authenticator = new OfflineAuthenticator("ZhaiSoul"), //离线启动，ZhaiSoul那儿为你要设置的游戏名
+                //Authenticator = new YggdrasilLogin("邮箱", "密码", true), // 正版启动，最后一个为是否twitch登录
+                Mode = LaunchMode.MCLauncher, //启动模式，这个我会在后面解释有哪几种
+                //Server = new ServerInfo { Address = "服务器IP地址", Port = "服务器端口" }, //设置启动游戏后，自动加入指定IP的服务器，可以不要
+                //ize = new WindowSize { Height = 768, Width = 1280 } //设置窗口大小，可以不要
+            });
+
         }
     }
 }
