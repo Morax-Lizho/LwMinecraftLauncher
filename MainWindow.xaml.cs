8dd2218e287c827a3bb0c91541948dcbaad643bc
Morax-Lizho <259333592@qq.com>
2023-02-12 13:25:55 +0800

    成功更换启动器核心为sml

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,8 +13,6 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Panuon.UI.Silver;
-using KMCCC.Launcher;
-using KMCCC.Authentication;
 using SquareMinecraftLauncher;
 using SquareMinecraftLauncherWPF;
 
@@ -28,27 +26,24 @@ namespace LwMinecraftLauncher
     {
         LoginUI.Offline Offline = new LoginUI.Offline();
         LoginUI.Mojang Mojang = new LoginUI.Mojang();
+        microsoft_launcher.MicrosoftAPIs microsoftAPIs = new microsoft_launcher.MicrosoftAPIs();
+        SquareMinecraftLauncher.Minecraft.Game game = new SquareMinecraftLauncher.Minecraft.Game();
         LoginUI.Microsoft Microsoft = new LoginUI.Microsoft();
         public int launchMode;
         SquareMinecraftLauncher.Minecraft.Tools tools = new SquareMinecraftLauncher.Minecraft.Tools();
-        public static LauncherCore Core = LauncherCore.Create();
+        
         public MainWindow()
         {
             InitializeComponent();
             //自动找版本
-            var versions = Core.GetVersions().ToArray();
+            var versions = tools.GetAllTheExistingVersion();
             versionCombo.ItemsSource = versions;
             //自动找java
             List<string> javaList = new List<string>();
-            foreach (string i in KMCCC.Tools.SystemTools.FindJava())
-            {
-                javaList.Add(i);
-            }
             javaList.Add(tools.GetJavaPath());
             javaCombo.ItemsSource = javaList;
             //初始选择
             versionCombo.SelectedItem = versionCombo.Items[0];
-            javaCombo.SelectedItem = javaCombo.Items[0];
             ContentControl.Content = new Frame
             {
                 Content = Offline
@@ -57,72 +52,43 @@ namespace LwMinecraftLauncher
         }
         public async void GameStart()
         {
-            LaunchOptions launchOptions = new LaunchOptions();
-            switch (launchMode)
+            try
             {
-                case 1:
-                    launchOptions.Authenticator = new OfflineAuthenticator(Offline.IdTextbox.Text);
-                    break;
-                case 2:
-                    launchOptions.Authenticator = new YggdrasilLogin(Mojang.Email.Text, Mojang.Password.Password,false);
-                    break;
-            }
-            launchOptions.MaxMemory = Convert.ToInt32(MemoryTextbox.Text);
-            if (versionCombo.Text != string.Empty&&javaCombo.Text != string.Empty&&MemoryTextbox.Text != string.Empty&&
-                (Offline.IdTextbox.Text != string.Empty||Mojang.Email.Text != string.Empty||Mojang.Password.Password != string.Empty))
-            {
-                try
+                if (versionCombo.Text != string.Empty && javaCombo.Text != string.Empty && MemoryTextbox.Text != string.Empty &&
+                    (Offline.IdTextbox.Text != string.Empty || Mojang.Email.Text != string.Empty || Mojang.Password.Password != string.Empty))
                 {
-                    if(launchMode != 3)
+                    switch (launchMode)
                     {
-                        Core.JavaPath = javaCombo.Text;
-                        var ver = (KMCCC.Launcher.Version)versionCombo.SelectedItem;
-                        launchOptions.Version = ver;
-                        var result = Core.Launch(launchOptions);
-                        if (!result.Success)
-                        {
-                            switch (result.ErrorType)
+                        case 1:
+                            await game.StartGame(versionCombo.Text,@"D:\Java\java8\bin\javaw.exe", Convert.ToInt32(MemoryTextbox.Text), Offline.IdTextbox.Text);
+                            break;
+                        case 2:
+                            await game.StartGame(versionCombo.Text, javaCombo.Text, Convert.ToInt32(MemoryTextbox.Text), Mojang.Email.Text, Mojang.Password.Password);
+                            break;
+                        case 3:
+                            microsoft_launcher.MicrosoftAPIs microsoftAPIs = new microsoft_launcher.MicrosoftAPIs();
+                            var v = Microsoft.MicrosoftWebBrowser.Source.ToString().Replace(microsoftAPIs.cutUri, string.Empty);
+                            var t = Task.Run(() =>
                             {
-                                case ErrorType.NoJAVA:
-                                    MessageBoxX.Show("Java错误，详细信息：" + result.ErrorMessage, "错误");
-                                    break;
-                                case ErrorType.AuthenticationFailed:
-                                    MessageBoxX.Show("登录错误，详细信息：" + result.ErrorMessage, "错误");
-                                    break;
-                                case ErrorType.UncompressingFailed:
-                                    MessageBoxX.Show("文件错误，详细信息：" + result.ErrorMessage, "错误");
-                                    break;
-                                default:
-                                    MessageBoxX.Show("未知错误，详细信息：" + result.ErrorMessage, "错误");
-                                    break;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        microsoft_launcher.MicrosoftAPIs microsoftAPIs = new microsoft_launcher.MicrosoftAPIs();
-                        var v = Microsoft.MicrosoftWebBrowser.Source.ToString().Replace(microsoftAPIs.cutUri,string.Empty);
-                        var t = Task.Run(() =>
-                        {
-                            return microsoftAPIs.GetAccessTokenAsync(v, false).Result;
-                        });
-                        await t;
-                        var v1 = microsoftAPIs.GetAllThings(t.Result.access_token, false);
-                        SquareMinecraftLauncher.Minecraft.Game game = new SquareMinecraftLauncher.Minecraft.Game();
-                        await game.StartGame(versionCombo.Text,javaCombo.Text,Convert.ToInt32(MemoryTextbox.Text),v1.name,v1.uuid,v1.mcToken,string.Empty,string.Empty);
+                                return microsoftAPIs.GetAccessTokenAsync(v, false).Result;
+                            });
+                            await t;
+                            var v1 = microsoftAPIs.GetAllThings(t.Result.access_token, false);
+                            await game.StartGame(versionCombo.Text, javaCombo.Text, Convert.ToInt32(MemoryTextbox.Text), v1.name, v1.uuid, v1.mcToken, string.Empty, string.Empty);
+                            break;
                     }
                 }
-                catch
+                else
                 {
-                    MessageBoxX.Show("启动失败", "错误");
+                    MessageBoxX.Show("信息未填充完整", "错误");
                 }
             }
-            else
+            catch
             {
-                MessageBoxX.Show("信息未填充完整", "错误");
+                MessageBoxX.Show("启动失败", "错误");
             }
         }
-
+    
         //启动按钮
         private void Button_Click(object sender, RoutedEventArgs e)
         {
